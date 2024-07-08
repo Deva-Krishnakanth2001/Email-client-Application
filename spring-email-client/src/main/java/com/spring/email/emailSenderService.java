075207e79c4c2696912746da5e9d08fe9c251ca9
Deva Krishnakanth <devakrishnakanth18@gmail.com>
2024-07-08 15:57:34 +0530

    Add Author

--- spring-email-client/src/main/java/com/spring/email/emailSenderService.java
+++ spring-email-client/src/main/java/com/spring/email/emailSenderService.java
@@ -13,46 +13,47 @@ import org.springframework.stereotype.Service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 
+// Author - Dkrishnakanth
+
 @Service
 @Component
 public class emailSenderService {
-	
+
 	@Autowired
 	private JavaMailSender mailSender;
-	
-	public void sendSimpleEmail(String toEmail , String body , String subject) {
-		
+
+	public void sendSimpleEmail(String toEmail, String body, String subject) {
+
 		SimpleMailMessage message = new SimpleMailMessage();
-		
+
 		message.setFrom("devakrishnakanth18@gmail.com");
 		message.setTo(toEmail);
 		message.setText(body);
 		message.setSubject(subject);
-		
+
 		mailSender.send(message);
 		System.out.println("Mail Sent...");
-		
+
 	}
-	
-	public void sendEmailWithAttachments(String toEmail , String body , String Subject,
-			 	String Attachments) throws MessagingException {
-		
+
+	public void sendEmailWithAttachments(String toEmail, String body, String Subject,
+			String Attachments) throws MessagingException {
+
 		MimeMessage msg = mailSender.createMimeMessage();
-		
-		MimeMessageHelper mimemessagehelper = new MimeMessageHelper(msg , true);
+
+		MimeMessageHelper mimemessagehelper = new MimeMessageHelper(msg, true);
 		mimemessagehelper.setFrom("devakrishnakanth18@gmail.com");
 		mimemessagehelper.setTo(toEmail);
 		mimemessagehelper.setSubject(Subject);
 		mimemessagehelper.setText(body);
-		
+
 		FileSystemResource fileSystemResource = new FileSystemResource(new File(Attachments));
-		
-		mimemessagehelper.addAttachment(fileSystemResource.getFilename() , fileSystemResource);
-		
+
+		mimemessagehelper.addAttachment(fileSystemResource.getFilename(), fileSystemResource);
+
 		mailSender.send(msg);
 		System.out.println("Mail send using attachments ... ");
-		
-	}
-	
-}
 
+	}
+
+}
